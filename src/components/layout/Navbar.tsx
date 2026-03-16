a0c89e379d16ad9ab0d14e6a7a4df953c284d4cd
Ian Sonoda <ian.mcfarland8@gmail.com>
2026-03-16 13:36:41 +0100

    feat: initial commit

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Dumbbell } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+
+export default function Navbar() {
+    return (
+        <header className="fixed top-0 left-0 right-0 z-50 border-b border-border bg-background/80 backdrop-blur-md">
+            <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
+                <Link 
+                    to="/"
+                    className="flex items-center gap-2 text-foreground">
+                    <Dumbbell className="w-6 h-6 text-accent"/>
+                    <span className="font-semibold text-lg">Gym AI</span>
+                </Link>
+
+                <nav>
+                    <>
+                    <Link to="/auth/sign-in">
+                        <Button> Sign In </Button>
+                    </Link>
+                    </>
+                </nav>
+            </div>
+        </header>
+    );
+}   
