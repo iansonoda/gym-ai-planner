3b170cf52f3722b407718595fef8f47938817e1f
Ian Sonoda <ian.mcfarland8@gmail.com>
2026-03-16 13:42:22 +0100

    feat: initial commit

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,5 @@
 import { Dumbbell } from "lucide-react";
 import { Link } from "react-router-dom";
-import { Button } from "../ui/button";
 
 export default function Navbar() {
     return (
@@ -15,9 +14,6 @@ export default function Navbar() {
 
                 <nav>
                     <>
-                    <Link to="/auth/sign-in">
-                        <Button> Sign In </Button>
-                    </Link>
                     </>
                 </nav>
             </div>
